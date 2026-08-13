b5cbc4ff65d55f7af3aeebf97aa205e7d79cbce1
Jared McCain <jaredmccain.jam@gmail.com>
2026-08-12 22:04:54 -0400

    .config/nvim, init.lua, lua/colorscheme.lua, lua/options.lua: create

---
+++ .config/nvim/lua/colorscheme.lua
@@ -0,0 +1,8 @@
+vim.cmd("colorscheme catppuccin")
+vim.api.nvim_set_hl(
+    0,
+    "Normal",
+    {
+        bg="None",
+    }
+)
---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,14 @@
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 2
+vim.o.expandtab = true
+vim.o.smarttab = true
+vim.o.autoindent = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.signcolumn = "auto"
+vim.o.cmdheight = 1
